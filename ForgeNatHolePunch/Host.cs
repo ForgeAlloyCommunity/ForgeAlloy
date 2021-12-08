faad2281158c6486409fe241f6ceb58274037efe
daneehAI <33600940+daneehAI@users.noreply.github.com>
2021-12-08 18:59:25 +0100

    Started porting and implementing NAT hole punching.
    I ported the Host struct from Remastered to Alloy.
    I laid out the workings of NAT hole punching in the ForgeNatHolePunch class in the form of comments, and will try porting from Remastered or implementing it from scratch later.

---
+++ ForgeNatHolePunch/Host.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using Forge.Networking.Players;
+
+//This is a port of NatHolePunch from Forge Networking Remastered, almost one-to-one.
+//I am not sure if this will be useful in Alloy's implementation, but I think it will.
+namespace ForgeNatHolePunch
+{
+	public struct Host
+	{
+		public ForgePlayer player;
+		public IPEndPoint endpoint;
+		public IPAddress address;
+		public string host;
+		public ushort port;
+
+		public Host(ForgePlayer player, string host, ushort port)
+		{
+			this.player = player;
+			this.host = host;
+			this.port = port;
+
+			if (IPAddress.TryParse(host, out address))
+			{
+				endpoint = new IPEndPoint(address, port);
+			}
+			else
+			{
+				throw new Exception("Invalid host address");
+			}
+		}
+	}
+}
